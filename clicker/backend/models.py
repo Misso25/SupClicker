7e89bd5007c9b4540f95ee5847907bee750e55ba
Misso25 <goldengoo@mail.ru>
2022-06-13 02:37:00 +0500

    added clicker

--- clicker/backend/models.py
+++ clicker/backend/models.py
@@ -6,3 +6,9 @@ class Core(models.Model):
     user = models.OneToOneField(User, null=False, on_delete=models.CASCADE)
     coins = models.IntegerField(default=0)
     click_power = models.IntegerField(default=1)
+
+    def click(self, commit=True):
+        self.coins += self.click_power
+        if commit:
+            self.save()
+        return self.coins
--- clicker/backend/views.py
+++ clicker/backend/views.py
@@ -2,16 +2,29 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from backend.forms import UserForm
-
-def index(request):
-    return render(request, 'index.html')
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import CoreSerializer
+from .models import Core
 
 @login_required
+def index(request):
+    core = Core.objects.get(user=request.user)
+    return render(request, 'index.html', {'core': core})
+
+@api_view(['GET'])
+def call_click(request):
+    core = Core.objects.get(user=request.user)
+    core.click()
+    return Response(CoreSerializer(core).data)
+
 def register(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         if user_form.is_valid():
             user = user_form.save()
+            core = Core(user=user)
+            core.save()
             login(request, user)
             return redirect('index')
         return render(request, 'register.html', {'user_form': user_form})
